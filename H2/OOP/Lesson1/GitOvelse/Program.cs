b4af2a80221854509db472466c732490a53dd5dc
christianbuchberg <cb@personalezonen.dk>
2026-08-27 10:19:18 +0200

    Update Program.cs

--- H2/OOP/Lesson1/GitOvelse/Program.cs
+++ H2/OOP/Lesson1/GitOvelse/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World");
+        Console.WriteLine("Lav en lille hilsen");
+
+        string input = Console.ReadLine() ?? string.Empty;
+        Console.WriteLine($"Hej {input}");
     }
 }

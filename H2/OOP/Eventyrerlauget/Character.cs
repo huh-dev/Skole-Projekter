2d9c958a488121133d780ae4be2945afbccdb1ff
huh <itzcbuchbergmc@gmail.com>
2026-09-04 08:20:00 +0200

    Refactored Character class to be abstract and added properties for Name, Level, MaxHp, and CurrentHp. Implemented methods for healing and taking damage. Updated IDamageable interface to include MaxHp, CurrentHp, and method signatures for TakeDamage and Heal, enhancing character management and damage handling.

--- H2/OOP/Eventyrerlauget/Character.cs
+++ H2/OOP/Eventyrerlauget/Character.cs
@@ -1,6 +1,40 @@
 namespace Eventyrerlauget;
 
-public class Character
+public abstract class Character
 {
+    public string Name { get; private set; }
+    public int Level { get; private set; }
+    public int MaxHp { get; private set; }
+    public int CurrentHp { get; private set; }
+
+    public Character(string name, int level, int maxHp)
+    {
+        Name = name;
+        Level = level;
+        MaxHp = maxHp;
+        CurrentHp = maxHp;
+    }
+
+    public abstract void Attack();
+
+    public void Heal(int amount)
+    {
+        CurrentHp += amount;
+
+        if (CurrentHp > MaxHp)
+        {
+            CurrentHp = MaxHp;
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        CurrentHp -= amount;
+
+        if (CurrentHp < 0)
+        {
+            CurrentHp = 0;
+        }
+    }
     
 }
--- H2/OOP/Eventyrerlauget/Interfaces/IDamageable.cs
+++ H2/OOP/Eventyrerlauget/Interfaces/IDamageable.cs
@@ -2,5 +2,10 @@ namespace Eventyrerlauget.Interfaces;
 
 public interface IDamageable
 {
-    
+    int MaxHp { get; }
+    int CurrentHp { get; }
+
+
+    void TakeDamage(int amount);
+    void Heal(int amount);
 }

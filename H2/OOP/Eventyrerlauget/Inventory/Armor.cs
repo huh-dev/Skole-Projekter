32123eefaa760247ce2a578139a781cac56a1a5b
christianbuchberg <cb@personalezonen.dk>
2026-09-07 09:13:34 +0200

    Add game setup and character creation mechanics
    Implemented the main game loop, including party creation, character selection, and battle mechanics. Introduced methods for handling player and monster turns, as well as spell casting and healing. Updated the Armor and Weapon classes to include constructors for better initialization.

--- H2/OOP/Eventyrerlauget/Inventory/Armor.cs
+++ H2/OOP/Eventyrerlauget/Inventory/Armor.cs
@@ -3,4 +3,9 @@ namespace Eventyrerlauget.Inventory;
 public class Armor : Item
 {
     public int armorClass { get; private set; }
+
+    public Armor(int armorClass)
+    {
+        this.armorClass = armorClass;
+    }
 }
--- H2/OOP/Eventyrerlauget/Inventory/Weapon.cs
+++ H2/OOP/Eventyrerlauget/Inventory/Weapon.cs
@@ -3,4 +3,9 @@ namespace Eventyrerlauget.Inventory;
 public class Weapon : Item
 {
     public int damage { get; private set; }
+
+    public Weapon(int damage)
+    {
+        this.damage = damage;
+    }
 }

e71de51bfea96eff962241a4189bffc7a197107f
huh <itzcbuchbergmc@gmail.com>
2026-09-09 16:00:55 +0200

    Add Hero class with properties and methods for gameplay mechanics; enhance Location struct with distance calculation

--- H2/OOP/Heltevagten/Hero.cs
+++ H2/OOP/Heltevagten/Hero.cs
@@ -1,6 +1,35 @@
+using Heltevagten.Enums;
+using Heltevagten.Incidents;
+
 namespace Heltevagten;
 
 public abstract class Hero
 {
-    
+    public string Name { get; private set; }
+    public int EnergyLevel { get; private set; }
+    public HeroState State { get; private set; }
+    public Location CurrentLocation { get; private set; }
+
+    public Hero(string name, int energyLevel, Location currentLocation)
+    {
+        Name = name;
+        EnergyLevel = energyLevel;
+        CurrentLocation = currentLocation;
+        State = HeroState.Available;
+    }
+
+    public string UseSignatureMove()
+    {
+        return $"{Name} uses their signature move!";
+    }
+
+    public void RechargeEnergyLevel(int amount)
+    {
+        EnergyLevel += amount;
+    }
+
+    public void UpdateState(HeroState newState)
+    {
+        State = newState;
+    }
 } 
--- H2/OOP/Heltevagten/Incidents/Location.cs
+++ H2/OOP/Heltevagten/Incidents/Location.cs
@@ -2,5 +2,11 @@ namespace Heltevagten.Incidents;
 
 public struct Location
 {
-    
+    public double x { get; private set; }
+    public double y { get; private set; }
+
+    public double CalculateDistance(Location other)
+    {
+        return Math.Sqrt(Math.Pow(x - other.x, 2) + Math.Pow(y - other.y, 2));
+    }
 }

2cb64fdfca9dbc4855f3609c4cb5cbdbd5316af9
christianbuchberg <cb@personalezonen.dk>
2026-09-14 09:54:51 +0200

    Add Product record and implement LINQ methods for product filtering
    Introduced a Product record to represent product details and added a static list of products. Implemented LINQ extension methods for filtering expensive products and demonstrated usage of lambda expressions and anonymous types in the Main method. Enhanced code organization and readability by structuring the program into distinct methods for different functionalities.

--- H2/LINQ/Dag1/Program.cs
+++ H2/LINQ/Dag1/Program.cs
@@ -1,9 +1,111 @@
 ﻿namespace H2.LINQ.Dag1;
 
+public record Product(string name, string category, int price);
 public class Program
 {
+
+    //Dictionary list because of very fast lookup times for specfic keys.
+
+
+    //Product name, Product category, Product price
+    private static readonly List<Product> _productsList = new()
+    {
+        new Product(name: "Gaming Laptop", category: "Computer", price: 12500),
+        new Product(name: "Office Laptop", category: "Computer", price: 7500),
+        new Product(name: "Keyboard", category: "Tilbehør", price: 650),
+        new Product(name: "Office Mus", category: "Tilbehør", price: 1100),
+        new Product(name: "4K Skærm", category: "Skærm", price: 4500),
+    };
+
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        // //Lambda Expressions
+        // LambdaExpressions();
+
+        // //Extension methods
+        var expensiveProducts = _productsList.ExpensiveProducts();
+
+        foreach (var product in expensiveProducts)
+        {
+            Console.WriteLine($"{product.name} costs {product.price} and is a {product.category}");
+        }
+
+        //Anonymous types
+        AnonymousTypes();
+
+    }
+
+
+    //MARK: Lambda expressions   
+    private static void LambdaExpressions()
+    {
+        //Lambda expression if a product costs more than 5000 (BOOL)
+        var isProductMoreThan5000 = (int price) => price > 5000;
+
+        //Lambda expression to get all products that costs more than 5000
+        var productsMoreThan5000 = _productsList.Where(p => isProductMoreThan5000(p.price));
+
+        // foreach (var product in productsMoreThan5000)
+        // {
+        //     Console.WriteLine($"{product.name} costs {product.price}");
+        // }
+
+        //Lambda expression to get products with a price over 10000 or have the category "Skærm"
+        var productsOver10000OrCategorySkærm = _productsList.Where(p => p.price > 10000 || p.category == "Skærm");
+
+        // foreach (var product in productsOver10000OrCategorySkærm)
+        // {
+        //     Console.WriteLine($"{product.name} costs {product.price} and is a {product.category}");
+        // }
+
+        // Lambda expression to get procuts that costs between 1000 and 10000 and does not have the category "Tilbehør"
+        var ProductsBetween1000And10000NotTilbehør = _productsList.Where(p => p.price > 1000 && p.price < 10000 && p.category != "Tilbehør");
+
+        // foreach (var product in ProductsBetween1000And10000NotTilbehør)
+        // {
+        //     Console.WriteLine($"{product.name} costs {product.price} and is a {product.category}");
+        // }
+
+        // Lambda expression to get procuts that costs between 1000 and 10000 and does not have the category "Tilbehør" and sort by price
+        var ProductsBetween1000And10000NotTilbehørSorted = _productsList.Where(p =>
+            p.price > 1000 && p.price < 10000 && p.category != "Tilbehør").OrderBy(p => p.price);
+        
+        // foreach (var product in ProductsBetween1000And10000NotTilbehørSorted)
+        // {
+        //     Console.WriteLine($"{product.name} costs {product.price} and is a {product.category}");
+        // }
+    }
+
+    //MARK: Anonymous types
+    private static void AnonymousTypes()
+    {
+        
+
+        var products = new[]
+        { 
+            new { Name = "Gaming Laptop", Category = "Computer", Price = 12500 },
+            new { Name = "Office Laptop", Category = "Computer", Price = 7500 },
+        };
+
+        var results = products.Select(p => new { p.Name, p.Category, p.Price });
+
+        foreach (var result in results)
+        {
+            Console.WriteLine($"{result.Name} costs {result.Price} and is a {result.Category}");
+        }
+
+    }
+
+
+}
+
+
+//MARK: Extension methods
+public static class ExtensionMethods
+{
+    public static IEnumerable<Product> ExpensiveProducts(this List<Product> products)
+    {
+        return products.Where(p => p.price > 5000);
     }
 }
